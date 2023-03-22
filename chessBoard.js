e167076b11d38506ec7bad84dbd50d6d6309f55a
Joao Paulo Chagas da Cruz <joaopauloch17@gmail.com>
2023-03-22 16:37:10 -0300

    chore: add a space after call a method

--- chessBoard.js
+++ chessBoard.js
@@ -6,7 +6,7 @@ const size = 16
 for(let a = 0; a < size; a++) {
     let [characterOne, characterTwo] = isEven(a) ? [' ', '#'] : ['#', ' ']
     for(let b = 0; b < size; b++) {
-        str += isEven(b)? characterOne : characterTwo
+        str += isEven(b) ? characterOne : characterTwo
     }
     str += '\n'
 }
