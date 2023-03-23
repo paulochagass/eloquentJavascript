669e83d510df99ff8f8cc001ac2c8d5932bb4b50
Joao Paulo Chagas da Cruz <joaopauloch17@gmail.com>
2023-03-23 04:57:26 -0300

    feat: minimun

---
+++ minimun.js
@@ -0,0 +1,11 @@
+/* 
+The previous chapter introduced the standard function Math.min that
+returns its smallest argument. We can build something like that now.
+Write a function min that takes two arguments and returns their minimum 
+*/
+
+function minimun(a, b) {
+    console.log(a < b ? a : b)
+}
+
+minimun(8, 7)
